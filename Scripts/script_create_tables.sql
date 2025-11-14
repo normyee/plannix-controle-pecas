8a315511d6985a220da64e92c8ee26352982f9f3
normyee <lunoqro@gmail.com>
2025-11-14 08:58:05 -0300

    :sparkles: feat: finaliza script sql para criar tabelas

--- Scripts/script_create_tables.sql
+++ Scripts/script_create_tables.sql
@@ -1,7 +1,40 @@
 CREATE TABLE obras (
-id int IDENTITY(1,1) PRIMARY KEY,
-nome_obra NVARCHAR(350) NOT NULL,
-sigla_obra VARCHAR(255) NULL,
-data_entrega DATE NOT NULL,
-status VARCHAR(20) DEFAULT 'ATIVO'
+id_obra INT IDENTITY(1,1) PRIMARY KEY,
+nome_obra NVARCHAR(200) NOT NULL,
+sigla_obra VARCHAR(20) NULL,
+data_entrega DATE NULL,
+status VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE pecas (
+id_peca INT IDENTITY(1,1) PRIMARY KEY,
+nome_peca NVARCHAR(255) NOT NULL,
+data DATE NOT NULL,
+volume_mcub DECIMAL(10,2) NULL,
+peso_kg DECIMAL(10,2) NULL,
+status VARCHAR(30) NOT NULL,
+id_obra INT NOT NULL FOREIGN KEY REFERENCES obras(id_obra)
+);
+
+CREATE TABLE regioes (
+id_regiao INT IDENTITY(1,1) PRIMARY KEY,
+nome_regiao NVARCHAR(255) NOT NULL,
+tipo_regiao VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE acabamentos (
+id_acabamento INT IDENTITY(1,1) PRIMARY KEY,
+id_peca INT NOT NULL FOREIGN KEY REFERENCES pecas(id_peca),
+data DATE NOT NULL,
+status VARCHAR(30) NOT NULL,
+id_regiao INT NOT NULL FOREIGN KEY REFERENCES regioes(id_regiao)
+);
+
+CREATE TABLE estoques (
+id_acabamento INT NOT NULL FOREIGN KEY REFERENCES acabamentos(id_acabamento),
+id_peca INT NOT NULL FOREIGN KEY REFERENCES pecas(id_peca),
+data DATE NOT NULL,
+status VARCHAR(30) NOT NULL,
+id_regiao INT NOT NULL FOREIGN KEY REFERENCES regioes(id_regiao),
+PRIMARY KEY (id_peca, id_acabamento)
 );
