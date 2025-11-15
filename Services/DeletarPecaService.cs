ef8ceb9e9303e9181c409cdf07ab9eafcb4bea9f
normyee <lunoqro@gmail.com>
2025-11-15 15:45:35 -0300

    feat: adiciona serviço de deletar peça

---
+++ Services/DeletarPecaService.cs
@@ -0,0 +1,19 @@
+﻿using ControlePecas.Repository;
+
+namespace ControlePecas.Services
+{
+    public class DeletarPecaService
+    {
+        private readonly DeletarPecaRepository _deletarPecaRepository;
+
+        public DeletarPecaService(DeletarPecaRepository deletarPecaRepository)
+        {
+            _deletarPecaRepository = deletarPecaRepository;
+        }
+
+        public void Executar(int codControle)
+        {
+           _deletarPecaRepository.Executar(codControle);
+        }
+    }
+}
