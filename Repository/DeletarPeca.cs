31167e13568e45a256de5180363315640c58069a
normyee <lunoqro@gmail.com>
2025-11-15 15:41:59 -0300

    feat: adiciona repositório para deletar peça

---
+++ Repository/DeletarPeca.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+namespace ControlePecas.Repository
+{
+    public class DeletarPeca
+    {
+        private readonly string _connectionString =
+            "server=localhost,1433;database=master;user id=sa;password=ControlePecas@2025;TrustServerCertificate=true;";
+
+        public void Executar(int codControle)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = "delete from pecas where id_peca = @codControle";
+
+                using (var cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@codControle", codControle);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
